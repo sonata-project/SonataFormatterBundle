bd098fe71d8576c5d3ea8bff30adba83cf7cf592
Ayhan Binici <ab@deltaweb.dk>
2014-04-11 22:28:47 +0200

    Hiding formatter, when only one formatter is defined.

--- Form/Type/FormatterType.php
+++ Form/Type/FormatterType.php
@@ -73,9 +73,13 @@ class FormatterType extends AbstractType
             $options['source_field_options']['property_path'] = $sourceField;
         }
 
-        $builder
-            ->add($formatField, 'choice', $options['format_field_options'])
-            ->add($sourceField, 'textarea', $options['source_field_options']);
+        if (count($this->pool->getFormatters()) == 1) {
+            $formatter = $this->pool->getFormatters();
+            $builder->add($formatField, 'hidden', array('data' => key($formatter)));
+        } else {
+            $builder->add($formatField, 'choice', $options['format_field_options']);
+        }
+        $builder->add($sourceField, 'textarea', $options['source_field_options']);
 
         /**
          * The listener option only work if the source field is after the current field
