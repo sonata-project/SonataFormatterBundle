sonata-project/SonataFormatterBundle

commit cc55a1ce10c966ba0db25018c418a01363849915

remove useless code

Form/Type/FormatterType.php
@@ -118,8 +118,6 @@ class FormatterType extends AbstractType
 
         $ckeditorConfiguration = array(
             'toolbar'       => array_values($options['ckeditor_toolbar_icons']),
-            'customConfig'  => false,
-            'contentsCss'   => false,
         );
 
         if ($options['ckeditor_context']) {
@@ -155,7 +153,6 @@ class FormatterType extends AbstractType
                  array('Maximize', 'Source')
             ),
             'ckeditor_context'          => null,
-            'ckeditor_basepath'         => 'bundles/sonataformatter/vendor/ckeditor',
             'format_field_options'      => array(
                 'choices'               => function (Options $options) use ($pool, $translator) {
                     $formatters = array();

Resources/views/Form/formatter.html.twig
@@ -24,8 +24,6 @@
                     {{ source_id }}_rich_instance.destroy();
                 }
 
-                CKEDITOR.basePath = "{{ asset(ckeditor_basepath) }}/";
-
                 var val = jQuery(this).val();
                 var appendClass = val;
                 switch(val) {

commit 237b8e0b34698ddc213d3db5316775c3ffa6622a

fix regression introduced by cc55a1ce10c966ba0db25018c418a01363849915

Form/Type/FormatterType.php
@@ -152,6 +152,7 @@ class FormatterType extends AbstractType
                  '-', 'Image', 'Link', 'Unlink', 'Table'),
                  array('Maximize', 'Source')
             ),
+            'ckeditor_basepath'         => 'bundles/sonataformatter/vendor/ckeditor',
             'ckeditor_context'          => null,
             'format_field_options'      => array(
                 'choices'               => function (Options $options) use ($pool, $translator) {

Formatter/Pool.php
@@ -15,7 +15,7 @@ use \Twig_Environment;
 use \Twig_Error_Syntax;
 use \Twig_Sandbox_SecurityError;
 
-use Symfony\Component\HttpKernel\Log\LoggerInterface;
+use Psr\Log\LoggerInterface;
 
 class Pool
 {