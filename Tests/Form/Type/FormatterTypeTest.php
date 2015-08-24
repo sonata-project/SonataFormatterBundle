9e51ffba5316b88fbba1335dee17a216a9dead0e
alexkvak <metalalisa@gmail.com>
2015-08-24 17:34:21 +0300

    Use default config if exists
    Added tests for default config usage
    
    Added tests for default config usage. CS fix
    
    Added tests for default config usage. CS fix
    
    Apply toolbar icons set to default config in any case
    
    Apply toolbar icons set to default config in any case. Test added.

--- Tests/Form/Type/FormatterTypeTest.php
+++ Tests/Form/Type/FormatterTypeTest.php
@@ -81,4 +81,100 @@ class FormatterTypeTest extends \PHPUnit_Framework_TestCase
 
         $type->buildForm($formBuilder, $options);
     }
+
+    public function testBuildViewWithDefaultConfig()
+    {
+        $pool = $this->getMockBuilder('Sonata\FormatterBundle\Formatter\Pool')->disableOriginalConstructor()->getMock();
+        $translator = $this->getMock('Symfony\Component\Translation\TranslatorInterface');
+        $configManager = $this->getMock('Ivory\CKEditorBundle\Model\ConfigManagerInterface');
+
+        $defaultConfig = 'default';
+        $defaultConfigValues = array('toolbar' => array('Button1'));
+        $configManager->expects($this->once())->method('getDefaultConfig')->will($this->returnValue($defaultConfig));
+        $configManager->expects($this->once())->method('hasConfig')->with($defaultConfig)->will($this->returnValue(true));
+        $configManager->expects($this->once())->method('getConfig')->with($defaultConfig)->will($this->returnValue($defaultConfigValues));
+
+        $type = new FormatterType($pool, $translator, $configManager);
+
+        /** @var \Symfony\Component\Form\FormView $view */
+        $view = $this->getMock('Symfony\Component\Form\FormView');
+        $view->vars['id'] = 'SomeId';
+        $view->vars['name'] = 'SomeName';
+        $form = $this->getMock('Symfony\Component\Form\FormInterface');
+        $type->buildView($view, $form, array(
+            'source_field'           => 'SomeField',
+            'format_field'           => 'SomeFormat',
+            'format_field_options'   => 'SomeOptions',
+            'ckeditor_context'       => null,
+            'ckeditor_basepath'      => '',
+            'ckeditor_toolbar_icons' => array(),
+        ));
+
+        $this->assertEquals($view->vars['ckeditor_configuration'], $defaultConfigValues);
+    }
+
+    public function testBuildViewWithoutDefaultConfig()
+    {
+        $pool = $this->getMockBuilder('Sonata\FormatterBundle\Formatter\Pool')->disableOriginalConstructor()->getMock();
+        $translator = $this->getMock('Symfony\Component\Translation\TranslatorInterface');
+        $configManager = $this->getMock('Ivory\CKEditorBundle\Model\ConfigManagerInterface');
+
+        $defaultConfig = 'default';
+        $configManager->expects($this->once())->method('getDefaultConfig')->will($this->returnValue($defaultConfig));
+        $configManager->expects($this->once())->method('hasConfig')->with($defaultConfig)->will($this->returnValue(false));
+
+        $type = new FormatterType($pool, $translator, $configManager);
+
+        $ckEditorToolBarIcons = array('Icon 1');
+
+        /** @var \Symfony\Component\Form\FormView $view */
+        $view = $this->getMock('Symfony\Component\Form\FormView');
+        $view->vars['id'] = 'SomeId';
+        $view->vars['name'] = 'SomeName';
+        $form = $this->getMock('Symfony\Component\Form\FormInterface');
+        $type->buildView($view, $form, array(
+            'source_field'           => 'SomeField',
+            'format_field'           => 'SomeFormat',
+            'format_field_options'   => 'SomeOptions',
+            'ckeditor_context'       => null,
+            'ckeditor_basepath'      => '',
+            'ckeditor_toolbar_icons' => $ckEditorToolBarIcons,
+        ));
+
+        $ckeditorConfiguration = array('toolbar' => $ckEditorToolBarIcons);
+        $this->assertEquals($view->vars['ckeditor_configuration'], $ckeditorConfiguration);
+    }
+
+    public function testBuildViewWithDefaultConfigAndWithToolbarIcons()
+    {
+        $pool = $this->getMockBuilder('Sonata\FormatterBundle\Formatter\Pool')->disableOriginalConstructor()->getMock();
+        $translator = $this->getMock('Symfony\Component\Translation\TranslatorInterface');
+        $configManager = $this->getMock('Ivory\CKEditorBundle\Model\ConfigManagerInterface');
+
+        $defaultConfig = 'default';
+        $defaultConfigValues = array('toolbar' => array('Button 1'));
+        $configManager->expects($this->once())->method('getDefaultConfig')->will($this->returnValue($defaultConfig));
+        $configManager->expects($this->once())->method('hasConfig')->with($defaultConfig)->will($this->returnValue(true));
+        $configManager->expects($this->once())->method('getConfig')->with($defaultConfig)->will($this->returnValue($defaultConfigValues));
+
+        $type = new FormatterType($pool, $translator, $configManager);
+
+        $ckEditorToolBarIcons = array('Icon 1');
+
+        /** @var \Symfony\Component\Form\FormView $view */
+        $view = $this->getMock('Symfony\Component\Form\FormView');
+        $view->vars['id'] = 'SomeId';
+        $view->vars['name'] = 'SomeName';
+        $form = $this->getMock('Symfony\Component\Form\FormInterface');
+        $type->buildView($view, $form, array(
+            'source_field'           => 'SomeField',
+            'format_field'           => 'SomeFormat',
+            'format_field_options'   => 'SomeOptions',
+            'ckeditor_context'       => null,
+            'ckeditor_basepath'      => '',
+            'ckeditor_toolbar_icons' => $ckEditorToolBarIcons,
+        ));
+
+        $this->assertEquals($view->vars['ckeditor_configuration'], array('toolbar' => $ckEditorToolBarIcons));
+    }
 }
