00cbac5aab778ca83149ab0ddee90fd34e45a310
Dmitry Vapelnik <dvapelnik@gmail.com>
2015-10-10 19:33:37 +0300

    Update FormatListener.php for support translations
    FormatListener.php updated for support translations like a2lix/TranslationFormBundle

--- Form/EventListener/FormatterListener.php
+++ Form/EventListener/FormatterListener.php
@@ -1,9 +1,10 @@
 <?php
 
 /*
- * This file is part of the Sonata Project package.
+ * This file is part of the Sonata project.
  *
  * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ * (c) Dmitry Vapelnik <dvapelnik@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -13,7 +14,9 @@ namespace Sonata\FormatterBundle\Form\EventListener;
 
 use Sonata\FormatterBundle\Formatter\Pool;
 use Symfony\Component\Form\FormEvent;
+use Symfony\Component\PropertyAccess\Exception\NoSuchPropertyException;
 use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
 
 class FormatterListener
 {
@@ -26,7 +29,7 @@ class FormatterListener
     protected $targetField;
 
     /**
-     * @param Pool   $pool
+     * @param Pool $pool
      * @param string $formatField
      * @param string $sourceField
      * @param string $targetField
@@ -45,16 +48,32 @@ class FormatterListener
      */
     public function postSubmit(FormEvent $event)
     {
-        $accessor = PropertyAccess::createPropertyAccessor();
-
-        $format = $accessor->getValue($event->getData(), $this->formatField);
-        $source = $accessor->getValue($event->getData(), $this->sourceField);
-
         // make sure the listener works with array
         $data = $event->getData();
+        $accessor = PropertyAccess::createPropertyAccessor();
 
-        $accessor->setValue($data, $this->targetField, $this->pool->transform($format, $source));
+        try {
+            $this->transformField($data, $accessor);
+        } catch (NoSuchPropertyException $e) {
+            $translations = $accessor->getValue($data, 'translations');
+
+            foreach ($translations as $translation) {
+                $this->transformField($translation, $accessor);
+            }
+        }
 
         $event->setData($data);
+    }
+
+    /**
+     * @param $data
+     * @param PropertyAccessor $accessor
+     */
+    private function transformField(&$data, $accessor)
+    {
+        $format = $accessor->getValue($data, $this->formatField);
+        $source = $accessor->getValue($data, $this->sourceField);
+
+        $accessor->setValue($data, $this->targetField, $this->pool->transform($format, $source));
     }
 }
