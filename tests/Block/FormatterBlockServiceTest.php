adb531a56cad33c6076561f3b186f92bfc390687
core23 <mail@core23.de>
2020-10-07 18:05:31 +0200

    Add support for sonata-project/block-bundle 4.x

--- tests/Block/FormatterBlockServiceTest.php
+++ tests/Block/FormatterBlockServiceTest.php
@@ -15,9 +15,9 @@ namespace Sonata\FormatterBundle\Tests\Block;
 
 use Sonata\BlockBundle\Block\BlockContext;
 use Sonata\BlockBundle\Model\Block;
-use Sonata\BlockBundle\Model\BlockInterface;
 use Sonata\BlockBundle\Test\BlockServiceTestCase;
 use Sonata\FormatterBundle\Block\FormatterBlockService;
+use Symfony\Component\HttpFoundation\Response;
 
 class FormatterBlockServiceTest extends BlockServiceTestCase
 {
@@ -32,18 +32,53 @@ class FormatterBlockServiceTest extends BlockServiceTestCase
             'template' => '@SonataFormatter/Block/block_formatter.html.twig',
         ]);
 
+        $response = new Response();
+
+        $this->twig->expects(static::once())->method('render')
+            ->with('@SonataFormatter/Block/block_formatter.html.twig', [
+                'settings' => $blockContext->getSettings(),
+                'block' => $blockContext->getBlock(),
+            ])
+            ->willReturn('TWIG_CONTENT');
+
+        $blockService = new FormatterBlockService($this->twig);
+        $blockService->execute($blockContext);
+    }
+
+    /**
+     * @group legacy
+     */
+    public function testExecuteWithDeprecatedTeamplating(): void
+    {
+        if (!class_exists('Sonata\BlockBundle\Block\Service\AbstractAdminBlockService')) {
+            static::markTestSkipped('Skipped for block-bundle 4');
+
+            return;
+        }
+
+        $block = new Block();
+
+        $blockContext = new BlockContext($block, [
+            'format' => 'richhtml',
+            'rawContent' => '<b>Insert your custom content here</b>',
+            'content' => '<b>Insert your custom content here</b>',
+            'template' => '@SonataFormatter/Block/block_formatter.html.twig',
+        ]);
+
+        $response = new Response();
+
         $blockService = new FormatterBlockService('block.service', $this->templating);
         $blockService->execute($blockContext);
 
         static::assertSame('@SonataFormatter/Block/block_formatter.html.twig', $this->templating->view);
 
         static::assertIsArray($this->templating->parameters['settings']);
-        static::assertInstanceOf(BlockInterface::class, $this->templating->parameters['block']);
+        static::assertInstanceOf(Block::class, $this->templating->parameters['block']);
     }
 
     public function testDefaultSettings(): void
     {
-        $blockService = new FormatterBlockService('block.service', $this->templating);
+        $blockService = new FormatterBlockService($this->twig);
         $blockContext = $this->getBlockContext($blockService);
 
         $this->assertSettings([
