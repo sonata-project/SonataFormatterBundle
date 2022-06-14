c7a6547282cefe63bffef62e81545129beb590e5
Jordi Sala Morales <jordism91@gmail.com>
2022-06-14 09:37:37 +0200

    Improve form extensions and add tests

--- tests/Form/Type/SimpleFormatterTypeTest.php
+++ tests/Form/Type/SimpleFormatterTypeTest.php
@@ -15,14 +15,14 @@ namespace Sonata\FormatterBundle\Tests\Form\Type;
 
 use FOS\CKEditorBundle\Config\CKEditorConfigurationInterface;
 use PHPUnit\Framework\MockObject\MockObject;
-use PHPUnit\Framework\TestCase;
 use Sonata\FormatterBundle\Form\Type\SimpleFormatterType;
-use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormExtensionInterface;
+use Symfony\Component\Form\PreloadedExtension;
+use Symfony\Component\Form\Test\TypeTestCase;
+use Symfony\Component\OptionsResolver\Exception\MissingOptionsException;
 
-class SimpleFormatterTypeTest extends TestCase
+final class SimpleFormatterTypeTest extends TypeTestCase
 {
-    private SimpleFormatterType $formType;
-
     /**
      * @var CKEditorConfigurationInterface&MockObject
      */
@@ -30,21 +30,56 @@ class SimpleFormatterTypeTest extends TestCase
 
     protected function setUp(): void
     {
-        parent::setUp();
-
         $this->ckEditorConfiguration = $this->createMock(CKEditorConfigurationInterface::class);
-        $this->formType = new SimpleFormatterType($this->ckEditorConfiguration);
+
+        parent::setUp();
+    }
+
+    public function testRequiredOptions(): void
+    {
+        $this->expectException(MissingOptionsException::class);
+        $this->expectExceptionMessage('An error has occurred resolving the options of the form "Sonata\FormatterBundle\Form\Type\SimpleFormatterType": The required option "format" is missing.');
+
+        $this->factory->create(SimpleFormatterType::class);
+    }
+
+    public function testSubmitForm(): void
+    {
+        $formData = 'data for ckeditor';
+
+        $form = $this->factory->create(SimpleFormatterType::class, null, [
+            'format' => 'default',
+        ]);
+
+        $form->submit($formData);
+
+        static::assertTrue($form->isSynchronized());
+        static::assertSame($formData, $form->getData());
+    }
+
+    public function testFormView(): void
+    {
+        $this->ckEditorConfiguration->expects(static::once())->method('getDefaultConfig')->willReturn('default');
+
+        $view = $this->factory->create(SimpleFormatterType::class, null, [
+            'format' => 'default',
+        ])->createView();
+
+        static::assertIsArray($view->vars['ckeditor_configuration']);
+        static::assertSame([], $view->vars['ckeditor_plugins']);
+        static::assertSame([], $view->vars['ckeditor_templates']);
+        static::assertSame([], $view->vars['ckeditor_style_sets']);
     }
 
     /**
-     * @doesNotPerformAssertions
+     * @return FormExtensionInterface[]
      */
-    public function testBuildForm(): void
+    protected function getExtensions(): array
     {
-        $formBuilder = $this->createMock(FormBuilderInterface::class);
-
-        $options = ['format' => 'format'];
-
-        $this->formType->buildForm($formBuilder, $options);
+        return [
+            new PreloadedExtension([
+                new SimpleFormatterType($this->ckEditorConfiguration),
+            ], []),
+        ];
     }
 }
