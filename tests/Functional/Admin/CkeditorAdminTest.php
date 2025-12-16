691ca183aed84a63a24e11eef86903b9ae76339d
David Maicher <mail@dmaicher.de>
2025-12-16 17:42:45 +0100

    drop PHP 8.1 and unmaintained Symfony versions

--- tests/Functional/Admin/CkeditorAdminTest.php
+++ tests/Functional/Admin/CkeditorAdminTest.php
@@ -61,7 +61,7 @@ final class CkeditorAdminTest extends WebTestCase
     private function prepareData(): void
     {
         $manager = self::getContainer()->get('doctrine.orm.entity_manager');
-        \assert($manager instanceof EntityManagerInterface);
+        static::assertInstanceOf(EntityManagerInterface::class, $manager);
 
         $media = new Media();
         $media->setName('name.jpg');
