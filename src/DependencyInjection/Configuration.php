a818ea5f96903a5c5fcb112b7a406a061a2132fe
Vincent Langlet <vincentlanglet@hotmail.fr>
2025-12-20 18:19:28 +0100

    Fix phpstan

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -23,6 +23,9 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
  */
 final class Configuration implements ConfigurationInterface
 {
+    /**
+     * @return TreeBuilder<'array'>
+     */
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('sonata_formatter');
